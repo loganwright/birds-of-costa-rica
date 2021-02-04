9394e2a2a0373f8708ae874adbfbefc76d2ae79e
Logan Wright <logan.william.wright@gmail.com>
2021-02-04 01:59:56 -0600

    Initial Commit

---
+++ BirdsOfCostaRica/BirdsOfCostaRicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdsOfCostaRicaApp.swift
+//  BirdsOfCostaRica
+//
+//  Created by Logan Wright on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct BirdsOfCostaRicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
